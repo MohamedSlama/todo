44c6d4af1bcc7d2a33abfc2a5b935c6f19385f78
mohamedslama <mohamed.slama@outlook.com>
2021-10-07 02:47:39 +0200

    changed notification screen

--- lib/ui/pages/notification_screen.dart
+++ lib/ui/pages/notification_screen.dart
@@ -1,20 +1,150 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:todo/ui/theme.dart';
 
 class NotificationScreen extends StatefulWidget {
-  const NotificationScreen({Key? key}) : super(key: key);
+  const NotificationScreen({Key? key, required this.payload}) : super(key: key);
+  final String payload;
 
   @override
   _NotificationScreenState createState() => _NotificationScreenState();
 }
 
 class _NotificationScreenState extends State<NotificationScreen> {
+  String _payload = '';
+
+  @override
+  void initState() {
+    super.initState();
+
+    //+ invoke parameter from main screen
+    _payload = widget.payload;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Container(
-        color: Colors.black,
-        child: const Text('This is the notification screen'),
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () => Get.back(),
+          icon: const Icon(Icons.arrow_back_ios),
+        ),
+        elevation: 0,
+        backgroundColor: context.theme.backgroundColor,
+        title: Text(
+          _payload.toString().split('|')[0],
+          style: TextStyle(color: Get.isDarkMode ? Colors.white : darkGreyClr),
+        ),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            const SizedBox(height: 20),
+            Column(
+              children: [
+                Text(
+                  'Hello, Adele',
+                  style: TextStyle(
+                    fontSize: 26,
+                    fontWeight: FontWeight.w900,
+                    color: Get.isDarkMode ? Colors.white : darkGreyClr,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  'You have a new reminder',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w300,
+                    color: Get.isDarkMode ? Colors.grey[100] : darkGreyClr,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 10),
+            Expanded(
+              child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 30,vertical: 10),
+                margin: const EdgeInsets.symmetric(horizontal: 30),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  color: primaryClr,
+                ),
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      //+ Title row
+                      Row(
+                        children: const [
+                          Icon(Icons.text_format,
+                              size: 35, color: Colors.white),
+                          SizedBox(width: 20),
+                          Text(
+                            'Title',
+                            style: TextStyle(color: Colors.white, fontSize: 30),
+                          )
+                        ],
+                      ),
+                      //- Horizontal space
+                      const SizedBox(height: 20),
+                      //? This is the _payload split first index from main.dart "Title"
+                      Text(_payload.toString().split('|')[0],
+                          style: const TextStyle(
+                              color: Colors.white, fontSize: 20)),
+                      //- Horizontal space
+                      const SizedBox(height: 20),
+                      //+ Description row
+                      Row(
+                        children: const [
+                          Icon(Icons.description,
+                              size: 35, color: Colors.white),
+                          SizedBox(width: 20),
+                          Text(
+                            'Description',
+                            style: TextStyle(color: Colors.white, fontSize: 30),
+                          )
+                        ],
+                      ),
+                      //- Horizontal space
+                      const SizedBox(height: 20),
+                      //? This is the _payload split Second index from main.dart "Description"
+                      Text(
+                        _payload.toString().split('|')[1],
+                        style:
+                            const TextStyle(color: Colors.white, fontSize: 20),
+                        textAlign: TextAlign.justify,
+                      ),
+                      //- Horizontal space
+                      const SizedBox(height: 20),
+                      //+ Date row
+                      Row(
+                        children: const [
+                          Icon(Icons.calendar_today_outlined,
+                              size: 35, color: Colors.white),
+                          SizedBox(width: 20),
+                          Text(
+                            'Date',
+                            style: TextStyle(color: Colors.white, fontSize: 30),
+                          )
+                        ],
+                      ),
+                      //- Horizontal space
+                      const SizedBox(height: 20),
+                      //? This is the _payload split third index from main.dart "Date"
+                      Text(
+                        _payload.toString().split('|')[2],
+                        style:
+                            const TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+          ],
+        ),
       ),
     );
   }
