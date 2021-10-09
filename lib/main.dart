2114e8b487bee616fb906d1bcc7ff096e835e0d9
mohamedslama <mohamed.slama@outlook.com>
2021-10-09 12:08:40 +0200

    Invoke notification service install on AndroidManifest.xml install on AppDelegate.swift add appicon.jpeg to android drawable initialize on main.dart initialize on initState on home_page.dart add test to appbar leading

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:todo/services/notification_services.dart';
 import 'package:todo/services/theme_services.dart';
 import 'ui/pages/home_page.dart';
 import 'ui/theme.dart';
 
-void main() {
+void main() async {
   runApp(const MyApp());
+  //? For Initializing notification
+  WidgetsFlutterBinding.ensureInitialized();
 }
 
 class MyApp extends StatelessWidget {
