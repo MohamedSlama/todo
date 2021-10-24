97ca5d14624200ff24d02fc9ea9b8b2ab7c72716
mohamedslama <mohamed.slama@outlook.com>
2021-10-24 02:57:12 +0200

    initialize database: invoke database helper class in db_helper.dart by adding: init , insert , delete , update , query edit task class in task.dart by initializing toMap and fromMap to save object inside db edit main.dart by calling DBHelper class and fix delay of theme by calling GetStorage

--- lib/models/task.dart
+++ lib/models/task.dart
@@ -1,5 +1,4 @@
 class Task {
-
   int? id;
   String? title;
   String? note;
@@ -11,15 +10,46 @@ class Task {
   int? remind;
   String? repeat;
 
-  Task(
-      {this.id,
-      this.title,
-      this.note,
-      this.isCompleted,
-      this.date,
-      this.startTime,
-      this.endTime,
-      this.color,
-      this.remind,
-      this.repeat});
+  Task({
+    this.id,
+    this.title,
+    this.note,
+    this.isCompleted,
+    this.date,
+    this.startTime,
+    this.endTime,
+    this.color,
+    this.remind,
+    this.repeat,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'note': note,
+      'isCompleted': isCompleted,
+      'date': date,
+      'startTime': startTime,
+      'endTime': endTime,
+      'color': color,
+      'remind': remind,
+      'repeat': repeat,
+    };
+  }
+
+  factory Task.fromMap(Map<String, dynamic> map) {
+    return Task(
+      id: map['id'] as int,
+      title: map['title'] as String,
+      note: map['note'] as String,
+      isCompleted: map['isCompleted'] as int,
+      date: map['date'] as String,
+      startTime: map['startTime'] as String,
+      endTime: map['endTime'] as String,
+      color: map['color'] as int,
+      remind: map['remind'] as int,
+      repeat: map['repeat'] as String,
+    );
+  }
 }
